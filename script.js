e5fa2cad00e4723876db490517c1f6a702a4ab58
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-03-11 22:26:01 +0200

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const message = 'suntsevyaroslav@gmail.com';
+console.log(message.length);
+console.log(message.includes('@'));
+
+const userName = prompt ('ваше імя');
+const payment = '300';
+const messageCost = `Дякуємо,${userName}! До сплати ${payment} гривень`;
+alert(messageCost)
+
+const productName = ' iPhone 13'
+const price = '29999';
+const  productPrice = `Товар: ${productName}. Вартість: ${price} грн.`;
+alert(productPrice)
